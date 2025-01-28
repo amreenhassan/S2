0a29b4bae9e8132d1590446d879efe4bdf362922
amreenhassan <amreenhassan13@gmail.com>
2025-01-28 13:11:50 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const lyrics = [
+    "Put myself to sleep just so I can get closer to you inside my dreams.",
+    "Promise me you won't regret me like the tattoos on my skin.",
+    "I can see it in your eyes' cause they never tell me lies.",
+    "We found each other, I helped you out of a broken place. You gave me comfort.",
+    "You're my favorite kind of night.",
+    "I said I didn't feel nothing baby, but I lied.",
+    "We don’t have to rush when you're alone with me."
+];
+
+document.getElementById('new-lyric').addEventListener('click', () => {
+    const randomLyric = lyrics[Math.floor(Math.random() * lyrics.length)];
+    document.getElementById('lyric-container').innerHTML = `<p>"${randomLyric}"</p>`;
+});
